c89e96ef48ff74d0bb4e98465b8546ac6bc70432
Sadia Afrin <116829514+meerror6d@users.noreply.github.com>
2023-07-09 08:28:34 +0600

    Update pl_sql.sql

--- pl_sql.sql
+++ pl_sql.sql
@@ -59,15 +59,87 @@ close product_cursor;
 end;
 /
 
+---array,if,else,loop use---
+
+set serveroutput on
+declare
+counter number:=1;
+pro_name products.product_name%type;
+type namearray is varray(5) of products.pro_name%type;
+a_name namearray:=namearray('laptop','phone','mouse','keyboard','watch');
+
+begin
+counter:=1;
+
+for x in 12..16
+loop
+select product_name into pro_name from products where products_id=100000;
+if pro_name='laptop' 
+then 
+dbms_output.put_line(pro_name);
+
+elsif pro_name='phone'
+then 
+dbms_output.put_line(pro_name);
+else
+dbms_output.put_line('other gadgets');
+
+end if;
+
+end loo;
+
+end;
 
 
+---procedure---
+set serveroutput on
+CREATE OR REPLACE PROCEDURE proc2(
+  var1 IN varchar1,
+  var2 OUT VARCHAR2,
+  var3 IN OUT NUMBER
+)
+AS
+  t_show CHAR(30);
+BEGIN
+  t_show := 'From procedure: ';
+  SELECT product_name INTO var2 FROM products WHERE category_id IN (SELECT category_id FROM product_category created_at = var1);
+  var3 := var1 + 1; 
+  DBMS_OUTPUT.PUT_LINE(t_show || var2 || ' is added in list at  ' || var1 || ' and price is  ' || var3);
+END;
+/
 
 
+set serveroutput on
+declare 
+created_date products.created_date%type;
+pro_name products.product_name%type;
+price number;
+begin
+proc2(pro_id,pro_name,price);
+end;
+/
 
 
+---function---
 
+set serveroutput on
+create or replace function fun(var1 in varchar) return varchar AS
+value products.product_name%type;
+begin
+  select product_name into pro_name from products where pro_id=var1; 
+   return value;
+end;
+/
 
+set serveroutput on
+declare 
+value varchar(20);
+begin
+value:=fun(10000);
+end;
+/
 
+  
 ---INTRODUCTION---
 
 set SERVEROUTPUT on;
